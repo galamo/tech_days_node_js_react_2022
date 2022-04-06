9c952021424de5d4ae50d1c16d965978cd2db844
Gal Amouyal <galamouy@Gals-MacBook-Pro.local>
2022-04-06 10:16:49 +0300

    example

---
+++ examples/callback/index.js
@@ -0,0 +1,28 @@
+const cars = [{
+    "Name": "chevrolet chevelle malibu",
+    "Miles_per_Gallon": 18,
+    "Cylinders": 8,
+    "Displacement": 307,
+    "Horsepower": 130,
+    "Weight_in_lbs": 3504,
+    "Acceleration": 12,
+    "Year": "1970-01-01",
+    "Origin": "USA"
+},
+{
+    "Name": "buick skylark 320",
+    "Miles_per_Gallon": 15,
+    "Cylinders": 8,
+    "Displacement": 350,
+    "Horsepower": 165,
+    "Weight_in_lbs": 3693,
+    "Acceleration": 11.5,
+    "Year": "1970-01-01",
+    "Origin": "USA"
+}]
+
+
+console.log("Start script")
+
+
+console.log("End script")
